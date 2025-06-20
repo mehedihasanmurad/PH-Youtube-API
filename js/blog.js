710452b51342219517584aad5d436028c6496f06
Mehedi Hasan Murad <mehedihasna500422@gamil.com>
2025-06-20 12:57:04 +0600

    dynamic cart and cart thumbnail added

--- js/blog.js
+++ js/blog.js
@@ -17,4 +17,36 @@ const displayCategories = (categories) => {
     });
 }
 
+//blogs data load korbe eikhane
+const loadBlogs = () => {
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+        .then(res => res.json())
+        .then(data => displayBlogs(data.videos))
+        .catch(error => console.log(error))
+}
+//blogs UI a dekhabo
+const displayBlogs = (blogs) => {
+    const blogsContainer = document.getElementById("blogs");
+    blogs.forEach((blog) => {
+        const cart = document.createElement("div");
+        cart.classList = "card shadow-lg";
+        cart.innerHTML = `
+        <figure>
+            <img class="h-50 w-full"
+            src=${blog.thumbnail}
+            alt="Shoes" />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">Card Title</h2>
+            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+            <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+        `;
+        blogsContainer.appendChild(cart);
+    })
+}
+
 loadCategories();
+loadBlogs();
