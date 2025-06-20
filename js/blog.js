48cc66df8be3e550e4f8584b5c50398eea03bb53
Mehedi Hasan Murad <mehedihasna500422@gamil.com>
2025-06-20 16:45:32 +0600

    something rendering added

--- js/blog.js
+++ js/blog.js
@@ -1,3 +1,12 @@
+//time pathanor jonno
+function getTimeString(time) {
+    const hours = parseInt(time / 3600);
+    const remainingSecond = time % 3600;
+    const minutes = parseInt(remainingSecond / 60);
+    const seconds = parseInt(remainingSecond / 60);
+    return `${hours}hrs ${minutes}min ${seconds}sec ago`
+}
+
 //data load korbo eikhane
 const loadCategories = () => {
     fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
@@ -28,20 +37,29 @@ const loadBlogs = () => {
 const displayBlogs = (blogs) => {
     const blogsContainer = document.getElementById("blogs");
     blogs.forEach((blog) => {
+        console.log(blog);
         const cart = document.createElement("div");
         cart.classList = "card shadow-lg";
         cart.innerHTML = `
-        <figure>
+        <figure class="relative">
             <img class="h-50 w-full"
             src=${blog.thumbnail}
             alt="Shoes" />
+            ${blog.others.posted_date?.length === 0 ? "" : `<span class="absolute right-2 bottom-2 bg-black text-white rounded p-1">${getTimeString(blog.others.posted_date)}</span>`}
+            
         </figure>
-        <div class="card-body">
-            <h2 class="card-title">Card Title</h2>
-            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-            <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
+        <div class="flex gap-3 px-0 py-10">
+            <div>
+                <img class="w-10 h-10 rounded-full" src=${blog.authors[0].profile_picture}/>
             </div>
+            <div>
+                <h2 class="font-bold">${blog.title}</h2>
+                <div class="flex items-center gap-3">
+                    <p>${blog.authors[0].profile_name}</p>
+                    ${blog.authors[0].verified === true ? `<img class="w-5" src="assest/verified.png" alt="">` : ''}
+                </div>
+            </div>
+            <div></div>
         </div>
         `;
         blogsContainer.appendChild(cart);
---
+++ js/test.js
@@ -0,0 +1,10 @@
+
+
+// function getTimeString(time) {
+//     const hours = parseInt(time / 3600);
+//     const remainingSecond = time % 3600;
+//     const minutes = parseInt(remainingSecond / 60);
+//     const seconds = parseInt(remainingSecond / 60);
+//     return `${hours}hrs ${minutes}min ${seconds}sec ago`
+// }
+// console.log(getTimeString(4450));
